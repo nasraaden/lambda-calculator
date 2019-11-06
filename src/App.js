eaffc4ba87487f4c98876ed2a251a93a3341fc98
Nasra <nasraaden@Nasras-MacBook-Pro.local>
2019-11-06 08:25:17 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display"
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -13,11 +17,32 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const [displayState, setDisplayState] = useState(0);
+  const [equation, setEquation] = useState([]);
+
+  // const test = (operator) => {
+  //   setEquation([displayState, operator]);
+  // }
+
+  // const equals = () => {
+  //   const number= equation[1];
+  //   const operator = equation[0];
+  //   setDisplayState(Number(number) + Number(displayState))
+  // }
+
   return (
     <div className="container">
       <Logo />
+      <Display displayState={displayState} />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <div className="left">
+          <Specials />
+          <Numbers setDisplayState = {setDisplayState} />
+        </div>
+        <div>
+          <Operators/>
+        </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
+
+  // const handleClick = () => {
+  // //   if (props.operator === "="){
+  // //     props.equals();
+  // //   } else {
+  // //     props.test(props.operator);
+  // //   }
+  // // }
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button> {props.operator.char} </button>
     </>
   );
 };
+
+export default OperatorButton;
+
